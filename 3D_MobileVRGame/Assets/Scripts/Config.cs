332d7ee8813219c75aed7741b608957484ede6cf
Asema Hassan <asema.hassan@gmail.com>
2017-04-01 20:08:55 +0200

    Removed extra lines from config

--- 3D_MobileVRGame/Assets/Scripts/Config.cs
+++ 3D_MobileVRGame/Assets/Scripts/Config.cs
@@ -1,56 +1,4 @@
-<<<<<<< Updated upstream
-﻿using UnityEngine;
-using System.Linq;
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
-using System;
-using System.IO;
 
-/// <summary>
-/// An extension method to shuffle the dictionary of any type
-/// </summary>
-public static class DictionaryExtensions
-{
-    public static Dictionary<TKey, TValue> Shuffle<TKey, TValue>(
-       this Dictionary<TKey, TValue> source)
-    {
-        System.Random r = new System.Random();
-        return source.OrderBy(x => r.Next())
-           .ToDictionary(item => item.Key, item => item.Value);
-    }
-}
-
-public enum PlayerState
-{
-    None = 0,
-    Idle,
-    Moving,
-    InsideRoom,
-    CenterPoint,
-    AutoTranslation,
-    OutsideRoom
-}
-
-public enum HMD
-{
-    None = -1,
-    OpenVR,
-    Oculus
-}
-
-public enum InputDevice
-{
-    None = -1,
-    XboxOne,
-}
-
-public enum GamePhase { //levels
-	Forest = 0,
-	City = 1,
-	Island = 2
-}
-=======
 ﻿using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
@@ -100,6 +48,14 @@ public static string ReplaceAtIndex (this string text, int index, char c)
 }
 */
 
+public enum GamePhase
+{
+	//levels
+	Forest = 0,
+	City = 1,
+	Island = 2
+}
+
 public enum PlayerState
 {
 	None = 0,
@@ -123,4 +79,3 @@ public enum InputDevice
 	None = -1,
 	XboxOne,
 }
->>>>>>> Stashed changes
