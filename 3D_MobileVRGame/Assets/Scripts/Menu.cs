0d595b6d163566cb16b3c415560d6b4070bfded6
Asema Hassan <asema.hassan@gmail.com>
2017-04-01 19:58:27 +0200

    Added freeman word game when interacting with mummy

--- 3D_MobileVRGame/Assets/Scripts/Menu.cs
+++ 3D_MobileVRGame/Assets/Scripts/Menu.cs
@@ -2,35 +2,40 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
-    public void StartDemo()
-    {
-        //To update list of sdk's for VR, this works only in editor
-      //  string[] priorityList = {HMD.OpenVR.ToString(), HMD.None.ToString()};
-      //  UnityEditorInternal.VR.VREditor.SetVREnabledDevices(UnityEditor.BuildTargetGroup.Standalone, priorityList);
-        StartCoroutine(LoadSceneWithDelay());
-    }
-    IEnumerator LoadSceneWithDelay()
-    {
-        yield return new WaitForSeconds(2.0f);
-        SceneManager.LoadScene(1);
-    }
-    public void QuitApplication()
-    {
+	public void StartDemo ()
+	{
+		//To update list of sdk's for VR, this works only in editor
+		//  string[] priorityList = {HMD.OpenVR.ToString(), HMD.None.ToString()};
+		//  UnityEditorInternal.VR.VREditor.SetVREnabledDevices(UnityEditor.BuildTargetGroup.Standalone, priorityList);
+		StartCoroutine (LoadSceneWithDelay ());
+	}
+
+	IEnumerator LoadSceneWithDelay ()
+	{
+		yield return new WaitForSeconds (2.0f);
+		SceneManager.LoadScene (1);
+	}
+
+	public void QuitApplication ()
+	{
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+		UnityEditor.EditorApplication.isPlaying = false;
 #endif
-        Application.Quit();
-    }
+		Application.Quit ();
+	}
 }
---
+++ 3D_MobileVRGame/Assets/Scripts/MummyController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MummyController : MonoBehaviour
+{
+	[SerializeField]
+	GameObject freemanGameRef = null;
+
+	[SerializeField]
+	GameObject mummyFences = null;
+
+	[SerializeField]
+	GameObject mummyObject = null;
+
+	private static control_script mummyCtrl = null;
+	private static Animation animFence = null;
+	// Use this for initialization
+	void Start ()
+	{
+		if (mummyFences != null)
+			animFence = mummyFences.GetComponent<Animation> ();
+
+		if (mummyCtrl == null) {
+			mummyCtrl = mummyObject.GetComponent<control_script> ();
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+
+		if (other.tag.Equals ("Player")) {
+			//Start the mummy convo system
+
+			//If player selects Yes! to Help then open the puzzle to guess word
+			ShowFreeManGame ();
+		}
+		
+	}
+
+	void ShowFreeManGame ()
+	{
+		if (!freemanGameRef.activeSelf) {
+			freemanGameRef.SetActive (true);
+		} else {
+			freemanGameRef.SetActive (false);
+		}
+			
+			
+	}
+
+	public static void OpenFences ()
+	{
+		if (animFence != null)
+			animFence.Play ();
+	}
+
+	public static IEnumerator playMummyAnimation (string animType, float delay)
+	{
+
+		yield return new WaitForSeconds (delay);
+		switch (animType) {
+		case "Run":
+			{
+				mummyCtrl.Run ();
+				break;
+			}
+		case "Attack":
+			{
+				mummyCtrl.Attack ();
+				break;
+			}
+		case "Death":
+			{
+				mummyCtrl.Death ();
+				break;
+			}
+		case "Damage":
+			{
+				mummyCtrl.Damage ();
+				break;
+			}
+			
+		}
+
+	}
+}
+
